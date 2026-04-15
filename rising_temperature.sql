6a7d5989063d40fc31c18a2baaf4288ffe0c76a3
mariam786 <M9ghani@gmail.com>
2026-04-15 15:36:24 +0530

    tough question,needs more practice

---
+++ rising_temperature.sql
@@ -0,0 +1,13 @@
+SELECT id
+FROM (
+    SELECT 
+        id,
+        recordDate,
+        temperature,
+        LAG(temperature) OVER (ORDER BY recordDate) AS prev_temp,
+        LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date
+    FROM Weather
+) t
+WHERE 
+    temperature > prev_temp
+    AND DATEDIFF(recordDate, prev_date) = 1;
