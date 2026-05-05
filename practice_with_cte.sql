f70cf81dec83958cccf36e0bcff1448034096a20
mariam786 <M9ghani@gmail.com>
2026-05-05 14:09:06 +0530

    practice question from sql

---
+++ practice_with_cte.sql
@@ -0,0 +1,20 @@
+WITH avg_salary_cte AS (
+    SELECT AVG(salary) AS avg_salary
+    FROM salaries
+),
+max_salary_per_employee AS (
+    SELECT emp_no, MAX(salary) AS max_salary
+    FROM salaries
+    GROUP BY emp_no
+),
+female_below_avg AS (
+    SELECT COUNT(*) AS highest_f_salaries_below_total_avg
+    FROM max_salary_per_employee m
+    JOIN employees e ON m.emp_no = e.emp_no
+    JOIN avg_salary_cte a
+    WHERE e.gender = 'F'
+      AND m.max_salary < a.avg_salary
+)
+
+SELECT highest_f_salaries_below_total_avg
+FROM female_below_avg;
